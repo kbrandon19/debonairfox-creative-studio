6828618f8ea2e05ffb9c340c8401d3ab94baf83e
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2024-03-12 22:29:57 -0500

    Custom colors in Tailwind, adding 404 and Coming Soon starter pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,68 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "sm": "375px",
+        "md": "1024px",
+        "lg": "1280px",
+        "2xl": "1440px",
+        "3xl": "1920px",
       },
     },
     extend: {
       colors: {
+        //custom colors
+        transparent: 'transparent',
+        current: 'currentColor',
+        white:'#FFFFFF',
+        black:{
+          main:'#000000',
+          text:'#212427'
+        },
+        teal:'#1D5B80',
+        //Light Blue
+        primary:{
+          default:'#6EA0C6',
+          '100':'#A8C6DD',
+          '200':'#8BB3D1',
+        },
+        //Green
+        secondary:{
+          default:'#50521D',
+          '100':'#969777',
+          '200':'#73754A',
+        },
+        //Tan
+        neutral:{
+          default:'#DBCEB4',
+          '100':'#E9E2D2',
+          '200':'#E2D8C3',
+        },
+        //Dark Blue
+        accent:{
+          default:'#24314B',
+          '100':'#7C8393',
+          '200':'#505A6F',
+        },
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: '#E9E2D2',
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
@@ -40,10 +71,7 @@ const config = {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
         },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
+
         popover: {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
@@ -75,6 +103,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
