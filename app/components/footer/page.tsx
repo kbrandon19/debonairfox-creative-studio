974d25ad0982a1884c3c4107df631ca23eb1bd8a
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2024-03-15 23:25:55 -0500

    Troubleshooting Footer grid issues, unable to transition from mobile to larger screens without sizing errors

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -6,24 +6,64 @@ import { Button } from "@/components/ui/button"
 function Footer() {
   return (
     <footer className='md:flex '>
-        <div className='w-full md:max-w-screen-2xl h-16 grid grid-cols-2 grid-rows-2 justify-between gap-y-4 pt-2.5 px-10 items-center'>
+        <div className='w-full md:max-w-screen-2xl h-16 grid grid-cols-2 grid-rows-2 smd:grid-cols-3  justify-between gap-y-4 px-10 items-center'>
 
           
 
+<div className=' grid col-span-1 smd:row-span-1'>
+                    
+                   <p className='text-xs uppercase'> 
+                    &copy; {new Date().getFullYear()} <span className='font-bold'> Kenneth Brandon </span>
+                    <span className=''>All Rights Reserved</span>
+                   </p>               
+    
+               
+            </div>
 
-
-            <div className='logo'>
+            <div className=' flex gap-x-6 smd:justify-center smd:col-span-2 smd:col-start-2 smd:col-end-3'>
                 <Link href="/">
                     <Image
-                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1710306493/Debonair%20Fox%20Creative%20Studio/logos/logo-03_enyv1m.svg"
-                    width={40}
-                    height={40}
-                    alt="DFCS Logo"
+                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1706837682/Debonair%20Fox%20Creative%20Studio/social-icons/LinkedIn_voqnk9.png"
+                    width={15}
+                    height={15}
+                    alt="LinkedIn"
+                    />
+                </Link>
+                <Link href="/">
+                    <Image
+                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1706837730/Debonair%20Fox%20Creative%20Studio/social-icons/Instagram_yyqg9d.png"
+                    width={15}
+                    height={15}
+                    alt="Instagram"
+                    />
+                </Link>
+                <Link href="/">
+                    <Image
+                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1706837820/Debonair%20Fox%20Creative%20Studio/social-icons/GitHub_ceujuk.png"
+                    width={15}
+                    height={15}
+                    alt="GitHub"
+                    />
+                </Link>
+                <Link href="/">
+                    <Image
+                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1706837822/Debonair%20Fox%20Creative%20Studio/social-icons/Email_zixsy3.png"
+                    width={15}
+                    height={15}
+                    alt="Gmail"
+                    />
+                </Link>
+                <Link href="/">
+                    <Image
+                    src="https://res.cloudinary.com/dujkjy2e2/image/upload/v1706837847/Debonair%20Fox%20Creative%20Studio/social-icons/Twitch_zc1buy.png"
+                    width={15}
+                    height={15}
+                    alt="Twitch"
                     />
                 </Link>
             </div>
 
-            <div>
+            <div className='grid justify-end smd:col-start-3'>
                 
                 <Link href="/">
                  <p>Top</p>
@@ -33,20 +73,8 @@ function Footer() {
               
            </div>
 
-            <div className='social-icons '>
-                    
-                   <p className='text-xs uppercase'> 
-                    &copy; {new Date().getFullYear()} <span className='font-bold'> Kenneth Brandon </span> <br/>
-                    <span className=''>All Rights Reserved</span>
-                   </p>               
-    
-               
-            </div>
+            
 
-           
-            <div className='logo'>
-
-            </div>
 
         </div>
 
