67e3b18ace33d7d3c8f3dc410efce21e5de280b1
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2024-11-06 19:58:28 -0500

    Parallax Scroll image testing

--- app/palScroll/page.tsx
+++ app/palScroll/page.tsx
@@ -65,7 +65,7 @@ function Page() {
       <div className="galleryWrapper" ref={gallery}>
         <Column images={[images[0], images[1], images[2]]} y={y} />
         <Column images={[images[3], images[4], images[5]]} y={y2} />
-        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[6], images[2], images[8]]} y={y3} />
         <Column images={[images[9], images[10], images[11]]} y={y4} />
       </div>
     </div>
